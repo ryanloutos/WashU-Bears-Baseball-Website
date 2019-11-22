849400627110852b59f6dbfef3d07084137c537b
Ryan Loutos <ryanloutos@ryans-mbp.dhcp.wustl.edu>
2019-11-22 11:56:02 -0600

    Working on login form

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, flash
 import mysql.connector
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret-key'
 
 db = mysql.connector.connect(
     host="localhost",
@@ -13,15 +17,44 @@ db = mysql.connector.connect(
 cur = db.cursor()
 
 cur.execute("SELECT * FROM Roster")
+
+
 players = ""
 
 for row in cur.fetchall() :
     players = players + row[0]
 
+cur.close()
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+
+
+# App routes
 @app.route("/")
 def index_page():
     return "index page"
 
+
+# FIX....CURRENT LOGINS: ryanloutos ryanloutos, mitchellblack mitchellblack
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        cur = db.cursor()
+        cur.execute("SELECT (password) FROM Login_Info WHERE username='{}'".format(form.username.data))
+        result = cur.fetchall()
+        if result[0][0] == form.password.data:
+            flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+            return render_template('index.html')
+        else
+            flash('Incorrect username or password')
+            return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html', title='Sign In', form=form)
 
 @app.route("/hello")
 def hello_world():
@@ -40,3 +73,4 @@ def show_template():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='3456')
+
--- database.sql
+++ database.sql
@@ -11,3 +11,10 @@ ALTER TABLE Roster ADD class varchar(10) not null;
 ALTER TABLE Roster ADD throws varchar(10) not null;
 ALTER TABLE Roster ADD number smallint;
 ALTER TABLE Roster ADD id mediumint not null auto_increment primary key; 
+
+CREATE TABLE Login_Info (
+	firstname varchar(30),
+    lastname varchar(30),
+	username varchar(30) not null primary key,
+    password varchar(30) not null
+);
