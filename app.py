f52b9fdd97b18ce25d931c67960825734734f2c4
Ryan Loutos <ryanloutos@Ryans-MacBook-Pro.local>
2019-11-24 12:45:15 -0600

    viewing/selecting outings

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, redirect, flash
 import mysql.connector
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -14,11 +14,53 @@ db = mysql.connector.connect(
     database="WashU_Pitching"
 )
 
+players = []
+
+
+cur = db.cursor()
+cur.execute("SELECT firstname, lastname FROM Roster")
+result = cur.fetchall()
+for player in result:
+    fullname = player[0]+" "+player[1]
+    players.append((fullname,fullname))
+    # players.append(fullname)
+cur.close()
+
+outings = [dict() for x in range(0)]
+outings_dropdown = []
+
+cur = db.cursor()
+cur.execute("SELECT * FROM Outings")
+result = cur.fetchall()
+cur.close()
+for outing in result:
+    date = outing[0]
+    opponent = outing[2]
+    season = outing[3]
+    firstname = outing[4]
+    lastname = outing[5]
+    this_outing = {
+        'pitcher': firstname+" "+lastname,
+        'date': date,
+        'season': season,
+        'opponent': opponent
+    }
+    this_outing_dropdown = date+" "+opponent
+    outings_dropdown.append((this_outing_dropdown, this_outing_dropdown))
+    outings.append(this_outing)
+
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class SelectPlayer(FlaskForm):
+    player_name = SelectField(u'Name', choices = players, validators=[DataRequired()])
+    player_outing = SelectField('Outing', choices = outings_dropdown, validators=[DataRequired()])
+    # which_statistic = SelectField('Stat', validators=[DataRequired()])
+    go = SubmitField('Go')
+
 
 # App routes
 @app.route("/")
@@ -38,7 +80,7 @@ def login():
             if result[0][0] == form.password.data:
                 # flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
                 cur.close()
-                return render_template('index.html')
+                return render_template('index.html', form=SelectPlayer())
             else:
                 flash('Incorrect username or password')
                 cur.close()
@@ -47,7 +89,6 @@ def login():
             flash('Incorrect username or password')
             cur.close()
             return render_template('login.html', title='Sign In', form=form)
-
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route("/hello")
@@ -60,9 +101,13 @@ def show_user(username):
     return f"Hello {username}"
 
 
-@app.route("/index")
-def show_template():
-    return render_template("index.html")
+@app.route("/index", methods=['GET', 'POST'])
+def outing():
+    form = SelectPlayer()
+    if form.validate_on_submit():
+        #do something here
+        return render_template("index.html", form=SelectPlayer())
+    return render_template("index.html", form=SelectPlayer())
 
 
 if __name__ == "__main__":
