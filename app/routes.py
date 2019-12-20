212769b3c1539bbeeb00e52082b80173a40a644d
Ryan Loutos <ryanloutos@Ryans-MacBook-Pro.local>
2019-12-19 22:59:28 -0600

    fdas

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, request, jsonify
+from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
