360785a17b393827b9931b405f092c007969f568
Ryan Loutos <ryanloutos@Ryans-MacBook-Pro.local>
2019-12-29 16:01:53 -0600

    added "retired" column to User

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ from flask_login import UserMixin
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     admin = db.Column(db.Boolean, index=True)
+    retired = db.Column(db.Boolean, index=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
