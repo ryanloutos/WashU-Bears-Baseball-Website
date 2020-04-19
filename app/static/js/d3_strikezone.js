aec1ad192f057541bf27a3e65d6fcc73a1736fbb
DMitchellBlack <mitchell.black@wustl.edu>
2020-04-19 01:37:35 -0500

    Begin dynamic scouting strikezone js class

--- app/static/js/d3_strikezone.js
+++ app/static/js/d3_strikezone.js
@@ -1,5 +1,28 @@
 //import d3 so can build objects out of it
 // var d3 = import("../packages/d3/d3.js");
+
+/**
+ * Zone constants based on scale defined in class strikezone
+ */
+var zone_constants = {
+    //x plane coords
+    x_left: -2,
+    x_zone_left: -0.833,
+    x_zone_left_third: -0.277,
+    x_zone_middle: 0,
+    x_zone_right_third: 0.277,
+    x_zone_right: 0.833,
+    x_right: 2,
+    //y plane coords
+    y_top: 4,
+    y_zone_top: 3.4,
+    y_zone_upper_third: 2.85,
+    y_zone_middle: 2.575,
+    y_zone_lower_third: 2.3,
+    y_zone_bottom: 1.75,
+    y_bottom: 0
+}
+
 /** Class representing a d3 strikezone as an object*/
 class strikezone {
 
@@ -223,6 +246,7 @@ class strikezone {
             }
 
             this.zone.append("rect")
+                .attr("id", "".concat("zone-highlight-", x, "-", y))
                 .attr("x", this.xScale(dx))
                 .attr("y", this.yScale(dy))
                 .attr("width", width)
@@ -393,5 +417,138 @@ class strikezone_legend {
 
             x_index += 20;
         }
+    }
+}
+
+class dynamic_scouting_strikezone extends strikezone{
+
+    /*
+    Inherited Variables:
+    this.width
+    this.height
+    this.pitch_colors
+    this.zone
+    this.g
+    this.xScale
+    this.yScale
+    */ 
+
+    constructor(div_id, width = 457, height = 457){
+        super(div_id, width, height);
+
+        //Add guidelines to base appearance of strikezone
+        this.g.append('path')
+            .attr('id', "zone-divisions")
+            .attr(
+                'd',
+                //draw vertical zone guidelines
+                'M ' + this.xScale(zone_constants.x_zone_left) + ', ' + this.yScale(zone_constants.y_top) + ' ' + 
+                'L ' + this.xScale(zone_constants.x_zone_left) + ', ' + this.yScale(zone_constants.y_bottom) + ' ' +
+                'M ' + this.xScale(zone_constants.x_zone_left_third ) + ', ' + this.yScale(zone_constants.y_top) + ' ' + 
+                'L ' + this.xScale(zone_constants.x_zone_left_third) + ', ' + this.yScale(zone_constants.y_bottom) + ' ' +
+                'M ' + this.xScale(zone_constants.x_zone_right_third) + ', ' + this.yScale(zone_constants.y_top) + ' ' + 
+                'L ' + this.xScale(zone_constants.x_zone_right_third) + ', ' + this.yScale(zone_constants.y_bottom) + ' ' +
+                'M ' + this.xScale(zone_constants.x_zone_right) + ', ' + this.yScale(zone_constants.y_top) + ' ' + 
+                'L ' + this.xScale(zone_constants.x_zone_right) + ', ' + this.yScale(zone_constants.y_bottom) + ' ' +
+                //draw horizontal zone guidelines
+                'M ' + this.xScale(zone_constants.x_left) + ', ' + this.yScale(zone_constants.y_zone_top) + ' ' + 
+                'L ' + this.xScale(zone_constants.x_right) + ', ' + this.yScale(zone_constants.y_zone_top) + ' ' +
+                'M ' + this.xScale(zone_constants.x_left) + ', ' + this.yScale(zone_constants.y_zone_upper_third) + ' ' + 
+                'L ' + this.xScale(zone_constants.x_right) + ', ' + this.yScale(zone_constants.y_zone_upper_third) + ' ' +
+                'M ' + this.xScale(zone_constants.x_left) + ', ' + this.yScale(zone_constants.y_zone_lower_third) + ' ' + 
+                'L ' + this.xScale(zone_constants.x_right) + ', ' + this.yScale(zone_constants.y_zone_lower_third) + ' ' +
+                'M ' + this.xScale(zone_constants.x_left) + ', ' + this.yScale(zone_constants.y_zone_bottom) + ' ' + 
+                'L ' + this.xScale(zone_constants.x_right) + ', ' + this.yScale(zone_constants.y_zone_bottom) + ' '
+            )
+            .style('stroke', 'grey')
+            .style('fill', 'none')
+            .style('stroke-width', 0.5);
+
+        //Setup the click highlighting of the regions of the zone
+        var zone_highlight_bools = {
+            "00": false,
+            "01": false,
+            "02": false,
+            "03": false,
+            "04": false,
+            "10": false,
+            "11": false,
+            "12": false,
+            "13": false,
+            "14": false,
+            "20": false,
+            "21": false,
+            "22": false,
+            "23": false,
+            "24": false,
+            "30": false,
+            "31": false,
+            "32": false,
+            "33": false,
+            "34": false,
+            "40": false,
+            "41": false,
+            "42": false,
+            "43": false,
+            "44": false
+        }
+        var self = this;
+        this.zone.on('click', function(){
+            var mouse = d3.mouse(this);
+            let x_coord, y_coord;
+
+            //get x grid region
+            if(mouse[0] < self.xScale(zone_constants.x_zone_left)){
+                //left of zone
+                x_coord = 0;
+            }
+            else if(mouse[0] < self.xScale(zone_constants.x_zone_left_third)){
+                //left third of zone
+                x_coord = 1;
+            }
+            else if(mouse[0] < self.xScale(zone_constants.x_zone_right_third)){
+                //middle third of zone
+                x_coord = 2;
+            }
+            else if(mouse[0] < self.xScale(zone_constants.x_zone_right)){
+                //right third of zone
+                x_coord = 3;
+            }
+            else{
+                //right of strikezone
+                x_coord = 4;
+            }
+
+            //get y grid region
+            if(mouse[1] < self.yScale(zone_constants.y_zone_top)){
+                //above zone region
+                y_coord = 0;
+            }
+            else if(mouse[1] < self.yScale(zone_constants.y_zone_upper_third)){
+                //above zone region
+                y_coord = 1;
+            }
+            else if(mouse[1] < self.yScale(zone_constants.y_zone_lower_third)){
+                //above zone region
+                y_coord = 2;
+            }
+            else if(mouse[1] < self.yScale(zone_constants.y_zone_bottom)){
+                //above zone region
+                y_coord = 3;
+            }
+            else {
+                //above zone region
+                y_coord = 4;
+            }
+            if(zone_highlight_bools["".concat(x_coord, y_coord)]){
+                //unhighlight the zone
+                self.zone.select("#".concat("zone-highlight-", x_coord, "-", y_coord)).remove();
+            } else{
+                //highlight the zone
+                self.highlightZonesDynamically([[x_coord, y_coord]]);
+            }
+            //switch bool
+            zone_highlight_bools["".concat(x_coord, y_coord)] = !zone_highlight_bools["".concat(x_coord, y_coord)];
+        });
     }
 }
